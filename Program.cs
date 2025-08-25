b670d892022a26c2d05e86847cc5e102050defab
karolrdg <karolinerodrigues12@outlook.com>
2025-08-25 17:01:31 -0300

    Refactor Pessoa instantiation and property assignment for clarity

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿using Fundamentos.Models;
 
-Pessoa pessoa = new Pessoa()
-{
-    Nome = "João",
-    Idade = 15
-};
+Pessoa pessoa1 = new Pessoa();
 
-pessoa.Apresentar();
+    pessoa1.Nome= "João";
+    pessoa1.Idade = 15;
+
+pessoa1.Apresentar();
+
+
