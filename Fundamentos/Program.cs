03b4536717b3ff3336ec08931a2e5797a943d108
karolrdg <karolinerodrigues12@outlook.com>
2025-09-04 15:59:28 -0300

    Update assembly information and regenerate binaries for Fundamentos and Fundamentos.Common

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,24 +1,71 @@
 ﻿using Fundamentos.Common.Models;
 
-int[] arrayInteiros = new int[5];
+List<string> estados = new List<string>();
 
-arrayInteiros[0] = 72;
-arrayInteiros[1] = 85;
-arrayInteiros[2] = 23;
-arrayInteiros[3] = 56;
-arrayInteiros[4] = 91;
+estados.Add("SP");
+estados.Add("MG");
+estados.Add("BA");
+estados.Add("RJ");
+Console.WriteLine($"Quantidade de estados: {estados.Count} - {estados.Capacity}");
 
-for (int contador = 0; contador < arrayInteiros.Length; contador++)
-{
-    Console.WriteLine($"Posição N° {contador} - {arrayInteiros[contador]}");
-}
+estados.Add("SC");
+Console.WriteLine($"Quantidade de estados: {estados.Count} - {estados.Capacity}");
 
-int valorPosicao = 0;
-foreach (int valor in arrayInteiros)
-{
-    Console.WriteLine($"Valor de cada posição do array: {valorPosicao} - {valor}");
-    valorPosicao++;
-}
+
+
+
+
+
+// for (int contador = 0; contador < estados.Count; contador++)
+// {
+//     Console.WriteLine($"Estado: {estados[contador]}");
+// }
+
+// int indice = 0;
+// foreach (string estado in estados)
+// {
+//     Console.WriteLine($"Estado: {estado}");
+//     indice++;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int[] arrayInteiros = new int[5];
+
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 85;
+// arrayInteiros[2] = 23;
+// arrayInteiros[3] = 56;
+// arrayInteiros[4] = 91;
+
+
+
+// for (int contador = 0; contador < arrayInteiros.Length; contador++)
+// {
+//     Console.WriteLine($"Posição N° {contador} - {arrayInteiros[contador]}");
+// }
+
+// // int valorPosicao = 0;
+// // foreach (int valor in arrayInteiros)
+// // {
+// //     Console.WriteLine($"Valor de cada posição do array: {valorPosicao} - {valor}");
+// //     valorPosicao++;
+// // }
 
 
 
